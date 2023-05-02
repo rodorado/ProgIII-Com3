f4b8ab9d8bd39993dac0f416a3f2b212d8c84400
Rocio Dorado <rocio_dorado12@hotmail.com>
2023-05-02 11:06:43 -0300

    clase pasada

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Mi primer app</h1>
+      <form className="formulario">
+        <p><input type="text" className="entrada" placeholder="Ingrese su Nombre" /></p>
+        <p><input type="text" className="entrada" placeholder="Ingrese su Apellido" /></p>
+        <input className="boton" type="submit" id="save" value="Guardar"/><br/>
+
+      </form>
     </div>
   );
 }
